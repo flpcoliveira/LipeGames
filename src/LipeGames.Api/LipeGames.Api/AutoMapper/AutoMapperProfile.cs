91606df7276cc4b70c4ebdf7aaf8a1df196a33e5
Felipe Cristo <felipe.cristo@hotmail.com>
2020-11-07 19:52:28 -0300

    Adiciona automapper profile

---
+++ src/LipeGames.Api/LipeGames.Api/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LipeGames.Dominio.Dto;
+using LipeGames.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LipeGames.Api.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Amigo, AmigoDto>().ReverseMap();
+        }
+    }
+}
