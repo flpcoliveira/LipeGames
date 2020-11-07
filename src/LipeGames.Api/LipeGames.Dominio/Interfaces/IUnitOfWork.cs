cf69343698ee10385c8e6c79417b70b16375d131
flpcoliveira <felipe.cristo@hotmail.com>
2020-11-07 02:02:57 -0300

    Cria a estrutura inicial da api
    Foi criada a estrutura inicial da api usando como base o modelo arquitetural DDD

---
+++ src/LipeGames.Api/LipeGames.Dominio/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LipeGames.Dominio.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+
+        void Rollback();
+    }
+}
---
+++ src/LipeGames.Api/LipeGames.Infraestrutura.Dados.EFCore/Contexto/EmprestimoContexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace LipeGames.Infraestrutura.Dados.EFCore.Contexto
+{
+    public class EmprestimoContexto: DbContext
+    {
+
+        public EmprestimoContexto(DbContextOptions<EmprestimoContexto> options): base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
