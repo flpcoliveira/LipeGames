e6dc2247ce54116574a1df26f4282f1af5a5f7c1
flpcoliveira <felipe.cristo@hotmail.com>
2020-11-07 10:01:37 -0300

    Altera visibilidade da classe

--- src/LipeGames.Api/LipeGames.Dominio/Entidades/Emprestimo.cs
+++ src/LipeGames.Api/LipeGames.Dominio/Entidades/Emprestimo.cs
@@ -5,11 +5,12 @@ using System.Text;
 
 namespace LipeGames.Dominio.Entidades
 {
-    class Emprestimo: EntidadeBase
+    public class Emprestimo: EntidadeBase
     {
         public int AmigoId { get; set; }
 
         public int JogoId { get; set; }
+
         public Amigo Amigo { get; set; }
 
         public Jogo Jogo { get; set; }
