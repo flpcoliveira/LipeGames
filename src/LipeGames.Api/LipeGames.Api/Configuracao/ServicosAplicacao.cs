f72b8189358bc67e5e5728492331fb7425340dd8
flpcoliveira <felipe.cristo@hotmail.com>
2020-11-07 09:36:38 -0300

    Cria classe de configuracao dos servicos de aplicacao

---
+++ src/LipeGames.Api/LipeGames.Api/Configuracao/ServicosAplicacao.cs
@@ -0,0 +1,23 @@
+﻿using LipeGames.Infraestrutura.Dados.EFCore.Contexto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LipeGames.Api.Configuracao
+{
+    public static class ServicosAplicacao
+    {
+        public static void Configurar(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EmprestimoContexto>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("EmprestimoContexto"));
+            });
+        }
+    }
+}
--- src/LipeGames.Api/LipeGames.Api/Startup.cs
+++ src/LipeGames.Api/LipeGames.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LipeGames.Api.Configuracao;
 using LipeGames.Infraestrutura.Dados.EFCore.Contexto;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,10 +30,7 @@ namespace LipeGames.Api
         {
             services.AddControllers();
 
-            services.AddDbContext<EmprestimoContexto>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("EmprestimoContexto"));
-            });
+            ServicosAplicacao.Configurar(services, Configuration);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
