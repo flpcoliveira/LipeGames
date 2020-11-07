d37a141520b64c9753986da0b1c080a0930ec74b
flpcoliveira <felipe.cristo@hotmail.com>
2020-11-07 14:19:53 -0300

    Adiciona datas do emprestimo
    Adiciona data do emprestimo e de devolução

--- src/LipeGames.Api/LipeGames.Infraestrutura.Dados.EFCore/Migrations/20201107133504_EmprestimoAdicionaDatas.Designer.cs
+++ src/LipeGames.Api/LipeGames.Infraestrutura.Dados.EFCore/Migrations/20201107133504_EmprestimoAdicionaDatas.Designer.cs
@@ -4,14 +4,16 @@ using LipeGames.Infraestrutura.Dados.EFCore.Contexto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LipeGames.Infraestrutura.Dados.EFCore.Migrations
 {
     [DbContext(typeof(EmprestimoContexto))]
-    partial class EmprestimoContextoModelSnapshot : ModelSnapshot
+    [Migration("20201107133504_EmprestimoAdicionaDatas")]
+    partial class EmprestimoAdicionaDatas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
