82e8373607583e3bead31590a16c63a145489dac
Felipe Cristo <felipe.cristo@hotmail.com>
2020-11-07 23:40:50 -0300

    Adiciona Validator

---
+++ src/LipeGames.Api/LipeGames.Dominio/Entidades/Validadores/AmigoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LipeGames.Dominio.Entidades.Validadores
+{
+    class AmigoValidator: AbstractValidator<Amigo>
+    {
+        public AmigoValidator()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty()
+                .WithMessage("Nome não pode estar vazio")
+                .MaximumLength(60)
+                .WithMessage("Nome deve ter no máximo 60 caracteres");
+        }
+    }
+}
