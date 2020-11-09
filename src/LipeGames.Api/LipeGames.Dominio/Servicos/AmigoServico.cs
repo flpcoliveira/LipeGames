6e8c61500cf0684340105625c64c476770d506c0
Felipe Cristo <felipe.cristo@hotmail.com>
2020-11-09 00:34:28 -0300

    Adiciona regras de validacao na inclusao e alteracao

--- src/LipeGames.Api/LipeGames.Dominio/Servicos/AmigoServico.cs
+++ src/LipeGames.Api/LipeGames.Dominio/Servicos/AmigoServico.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using LipeGames.Dominio.Dto;
 using LipeGames.Dominio.Entidades;
 using LipeGames.Dominio.Excecoes;
@@ -6,72 +7,95 @@ using LipeGames.Dominio.Interfaces.Repositorios;
 using LipeGames.Dominio.Interfaces.Servicos;
 using LipeGames.Dominio.Interfaces.UnidadeTrabalho;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LipeGames.Dominio.Servicos
 {
     public class AmigoServico : IAmigoServico
     {
+
+        const string MSG_ERRO_VALIDACAO = "Ocorreu um problema de validação das informações do amigo";
+        const string MSG_AMIGO_NAO_ENCONTRADO = "Amigo com código {0} não encontrado";
+
         private readonly IUnidadeTrabalho _unidadeTrabalho;
         private readonly IAmigoRepositorio _repositorio;
         private readonly IMapper _mapper;
+        private readonly IValidator<Amigo> _validator;
 
-        public AmigoServico(IUnidadeTrabalho unidadeTrabalho, IAmigoRepositorio repositorio, IMapper mapper)
+        public AmigoServico(IUnidadeTrabalho unidadeTrabalho, IAmigoRepositorio repositorio, IMapper mapper, IValidator<Amigo> validator)
         {
             _unidadeTrabalho = unidadeTrabalho;
             _repositorio = repositorio;
             _mapper = mapper;
+            _validator = validator;
         }
 
         public async Task<AmigoDto> Alterar(int id, AmigoDto amigo)
         {
-            var entidade = await _repositorio.Detalhar(id);
-            if (entidade == null) throw new EntidadeNaoEncotradaException($"Amigo com código {id} não encontrado");
+            try
+            {
+                var entidade = await RecuperarEntidadePorId(id);
 
-            _mapper.Map(amigo, entidade);
+                _mapper.Map(amigo, entidade);
+                await _validator.ValidateAndThrowAsync(entidade);
 
-            entidade = _repositorio.Alterar(entidade);
-            await _unidadeTrabalho.Commit();
+                entidade = _repositorio.Alterar(entidade);
+                await _unidadeTrabalho.Commit();
 
-            amigo = _mapper.Map(entidade, amigo);
-            return amigo;
+                amigo = _mapper.Map(entidade, amigo);
+                return amigo;
+
+            }
+            catch (ValidationException e)
+            {
+                var erros = e.Errors.ToDictionary(mensagem => mensagem.PropertyName, mensagem => mensagem.ErrorMessage);
+                throw new RegraNegocioExcecao(MSG_ERRO_VALIDACAO, erros);
+            }
         }
 
         public async Task<AmigoDto> Criar(AmigoDto amigo)
         {
-            var entidade = _mapper.Map<Amigo>(amigo);            
+            try
+            {
+                var entidade = _mapper.Map<Amigo>(amigo);
+                await _validator.ValidateAndThrowAsync(entidade);
 
-            entidade = await _repositorio.Criar(entidade);
-            await _unidadeTrabalho.Commit();
+                entidade = await _repositorio.Criar(entidade);
+                await _unidadeTrabalho.Commit();
 
-            return _mapper.Map(entidade, amigo);
+                return _mapper.Map(entidade, amigo);
+            }
+            catch (ValidationException e)
+            {
+                var erros = e.Errors.ToDictionary(mensagem => mensagem.PropertyName, mensagem => mensagem.ErrorMessage);
+                throw new RegraNegocioExcecao(MSG_ERRO_VALIDACAO, erros);
+            }
         }
 
         public async Task<AmigoDto> Detalhar(int id)
         {
-            var entidade = await _repositorio.Detalhar(id);
-            if (entidade == null) throw new EntidadeNaoEncotradaException($"Amigo com código {id} não encontrado");
-
+            var entidade = await RecuperarEntidadePorId(id);
             return _mapper.Map<AmigoDto>(entidade);
         }
 
         public async Task Excluir(int id)
         {
             var jogo = await RecuperarEntidadePorId(id);
-             _repositorio.Excluir(jogo);
+            _repositorio.Excluir(jogo);
             await _unidadeTrabalho.Commit();
         }
 
         public async Task<IEnumerable<AmigoDto>> Listar()
         {
             var listaAmigos = await _repositorio.Listar();
-            return _mapper.Map <IEnumerable<Amigo>, IEnumerable<AmigoDto>>(listaAmigos);
+            return _mapper.Map<IEnumerable<Amigo>, IEnumerable<AmigoDto>>(listaAmigos);
         }
 
         private async Task<Amigo> RecuperarEntidadePorId(int id)
         {
             var entidade = await _repositorio.Detalhar(id);
-            if (entidade == null) throw new EntidadeNaoEncotradaException($"Amigo com código {id} não encontrado");
+            if (entidade == null) throw new EntidadeNaoEncotradaException(MSG_AMIGO_NAO_ENCONTRADO.Replace("{0}", id.ToString()));
             return entidade;
         }
     }
