95e19d7c64d9d55b2ee0cf7c0a171c016f43b96a
Felipe Cristo <felipe.cristo@hotmail.com>
2020-11-10 19:44:27 -0300

    Adiciona teste para a entidade jogo

---
+++ src/LipeGames.Api/LipeGames.Teste/JogoTest.cs
@@ -0,0 +1,23 @@
+﻿using LipeGames.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LipeGames.Teste
+{
+    public class JogoTest
+    {
+        [Fact]
+        public void Test_Create_Basic_Instance()
+        {
+            var idEsperado = 1;
+            var nomeEsperado = "João Santo Cristo";
+            var entidade = new Jogo { Id = 1, Nome = nomeEsperado };
+
+            Assert.Equal(idEsperado, entidade.Id);
+            Assert.Equal(nomeEsperado, entidade.Nome);
+
+        }
+    }
+}
